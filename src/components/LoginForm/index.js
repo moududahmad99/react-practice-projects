f0eb0c97cfbb6af9e32b7831a29f8681fad5f274
Moudud Ahmad <sabbirahmedmoudud@gmail.com>
2023-04-27 22:42:15 +0600

    Design UI LoginForm

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import './style.css'
 
 function LoginForm() {
     const [name, setName] = useState('');
@@ -17,24 +18,26 @@ function LoginForm() {
 
     return (
         <React.Fragment>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label>Name:</label>
-                    <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-                </div>
-                <div>
-                    <label>Email: </label>
-                    <input type="email" value={email} onChange={(e) => {
-                        setEmail(e.target.value)
-                    }} />
-                </div>
-                <div>
-                    <label>Password:</label>
-                    <input type="password" value={password} onChange={(e) => {
-                        setPassword(e.target.value)
-                    }} />
-                    <button type='submit'>Submit Info</button>
-                </div>
+            <form className='form' onSubmit={handleSubmit}>
+                <div className='form-wrapper'>
+                        <div className='form-inner'>
+                            <label>Name:</label>
+                            <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+                        </div>
+                        <div className='form-inner'>
+                            <label>Email: </label>
+                            <input type="email" value={email} onChange={(e) => {
+                                setEmail(e.target.value)
+                            }} />
+                        </div>
+                        <div className='form-inner'>
+                            <label>Password:</label>
+                            <input type="password" value={password} onChange={(e) => {
+                                setPassword(e.target.value)
+                            }} />
+                        </div>
+                        <button type='submit' className='button'>Submit Info</button>
+                    </div>
             </form>
         </React.Fragment>
     )
